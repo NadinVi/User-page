13b783b6729dc068429a8448bec954e9082c24cc
NadinVi <vinokhodova.nadin@gmail.com>
2024-10-14 08:14:09 +0200

    first commit

---
+++ js/link.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll('.header__menu .link');
+
+links.forEach(link => {
+    link.addEventListener('click', function(element) {
+    
+        links.forEach(item => item.classList.remove('active'));
+        element.target.classList.add('active');
+    });
+});
